38e773283dc6472156079ec3f71f2c6e210c9dae
khattakasim708-star <khattakasim708@gmail.com>
2025-11-23 17:18:17 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer: React.FC = () => (
-  <footer className="py-6 text-center text-gray-400">
+  <footer className="py-6 text-center text-gray-400 bg-gray-900">
     &copy; 2025 Asim Khattak. All rights reserved.
   </footer>
 );
