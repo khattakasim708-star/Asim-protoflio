8635b7d1c0e78e08b0d1948925a43c268aea7370
khattakasim708-star <khattakasim708@gmail.com>
2025-11-23 18:23:32 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/index.css'; // <-- correct path to CSS
+import './styles/index.css'; // Make sure this matches the folder
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
