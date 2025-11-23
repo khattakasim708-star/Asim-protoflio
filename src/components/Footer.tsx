b93c11ae2c02ffb00ed5efe665a0e94ac7c1b798
khattakasim708-star <khattakasim708@gmail.com>
2025-11-23 17:07:19 +0500

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <footer className="py-6 text-center text-gray-400">
+    &copy; 2025 Asim Khattak. All rights reserved.
+  </footer>
+);
+
+export default Footer;
